d33354e6960a7c29cd37c19536f850da02dc62eb
Stephen Cavaliere <stephencavaliere@gmail.com>
2020-07-04 11:39:47 -0400

    feat: add -rep command

--- src/bot.ts
+++ src/bot.ts
@@ -3,6 +3,7 @@ import 'reflect-metadata';
 import { Client, Message } from 'discord.js';
 import { createConnection } from 'typeorm';
 
+import { MinusRepCommand } from './commands/minus-rep';
 import { PlusRepCommand } from './commands/plus-rep';
 import { Vouch } from './entities/vouch';
 
@@ -16,8 +17,10 @@ async function main() {
   });
 
   const plusRepCommand = new PlusRepCommand(client, connection.getRepository(Vouch));
+  const minusRepCommand = new MinusRepCommand(client, connection.getRepository(Vouch));
 
   client.on(plusRepCommand.on, async (message: Message) => await plusRepCommand.execute(message));
+  client.on(minusRepCommand.on, async (message: Message) => await minusRepCommand.execute(message));
 
   client.login(process.env.DISCORD_TOKEN);
 }
