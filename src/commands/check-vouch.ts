d565a2b60c8aa367641a1dcc61c9846f5a7a9b07
Stephen Cavaliere <stephencavaliere@gmail.com>
2020-07-04 20:09:31 -0400

    feat: add embed colors and how to improve summary message

--- src/commands/check-vouch.ts
+++ src/commands/check-vouch.ts
@@ -5,6 +5,12 @@ import { Repository } from 'typeorm';
 import { Vouch } from '../entities/vouch';
 import { MessageCommand } from './command.base';
 
+const colors = {
+  positive: 752646,
+  neutral: 12501004,
+  negative: 11472912,
+};
+
 export class CheckVouchCommand extends MessageCommand {
   constructor(private client: Client, private vouchRepository: Repository<Vouch>) {
     super({
@@ -25,31 +31,22 @@ export class CheckVouchCommand extends MessageCommand {
       return;
     }
 
-    const vouches: Vouch[] = await this.vouchRepository.find({ vouchedId: user });
     const userInfo = await this.client.users.fetch(user);
 
+    const vouches: Vouch[] = await this.vouchRepository.find({ vouchedId: user });
+    const vouchSummary = this.getVouchSummary(vouches);
+
     const embed = new MessageEmbed()
-      .setColor(752646)
-      .setTitle(`Report for ${userInfo.username}`)
-      .addField(
-        'Total Vouches',
-        vouches.reduce((sum, vouch) => vouch.amount + sum, 0),
-        true
-      )
-      .addField(
-        'Positive',
-        vouches.filter((vouch) => vouch.amount > 0).reduce((sum, vouch) => vouch.amount + sum, 0),
-        true
-      )
-      .addField(
-        'Negative',
-        vouches.filter((vouch) => vouch.amount < 0).reduce((sum, vouch) => vouch.amount + sum, 0),
-        true
-      )
-      .addField('Unique Vouchers', vouches.reduce((set, vouch) => set.add(vouch.voucherId), new Set()).size, true)
+      .setColor(this.getEmbedColor(vouchSummary))
+      .setTitle(`${userInfo.username}#${userInfo.discriminator}'s Report`)
+      .addField('Total Vouches', vouchSummary.positive - vouchSummary.negative, true)
+      .addField('Positive', vouchSummary.positive, true)
+      .addField('Negative', vouchSummary.negative, true)
+      .addField('Unique Vouchers', vouchSummary.uniqueVouchers, true)
       .addField('\u200b', '\u200b', true)
       .addField('\u200b', '\u200b', true)
       .addField('Account Age', `created ${formatDistanceToNow(userInfo.createdAt, { includeSeconds: true })} ago`, true);
+
     try {
       const guildUserInfo = message.guild?.member(userInfo.id);
       if (guildUserInfo?.joinedAt != null) {
@@ -61,7 +58,29 @@ export class CheckVouchCommand extends MessageCommand {
       embed.addField('Server Age', 'User is not currently on our server', true);
     }
 
+    embed.addField('---', `Have issues with the report? Let us know in <#${process.env.SUGGESTIONS_CHANNEL_ID}>!`);
+
     await message.channel.send(embed);
+  }
+
+  private getEmbedColor(vouchSummary: VouchSummary): number {
+    const total = vouchSummary.positive - vouchSummary.negative;
+
+    if (total > 0) {
+      return colors.positive;
+    } else if (total < 0) {
+      return colors.negative;
+    } else {
+      return colors.neutral;
+    }
+  }
+
+  private getVouchSummary(vouches: Vouch[]): VouchSummary {
+    return {
+      uniqueVouchers: vouches.reduce((set, vouch) => set.add(vouch.voucherId), new Set()).size,
+      positive: vouches.filter((vouch) => vouch.amount > 0).reduce((sum, vouch) => vouch.amount + sum, 0),
+      negative: Math.abs(vouches.filter((vouch) => vouch.amount < 0).reduce((sum, vouch) => vouch.amount + sum, 0)),
+    };
   }
 
   private getUserFromMessage(message: string): string | null {
@@ -72,4 +91,10 @@ export class CheckVouchCommand extends MessageCommand {
 
     return matches[1].trim();
   }
+}
+
+interface VouchSummary {
+  uniqueVouchers: number;
+  positive: number;
+  negative: number;
 }
